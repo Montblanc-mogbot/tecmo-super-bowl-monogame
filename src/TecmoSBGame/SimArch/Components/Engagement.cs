2052566b0a7e3c3f5d352f1fea7dd7eaff75f9e3
Montblanc <montblanc.mogbot@gmail.com>
2026-03-19 12:45:26 -0400

    simarch: port engagement component

--- src/TecmoSBGame/SimArch/Components/Engagement.cs
+++ src/TecmoSBGame/SimArch/Components/Engagement.cs
@@ -2,6 +2,8 @@ namespace TecmoSBGame.SimArch.Components;
 
 /// <summary>
 /// Tracks a short engagement cooldown so blockers/defenders don't re-engage every tick.
+///
+/// Ported from: ArchiveMge/Components/BehaviorInterruptComponents.cs (EngagementComponent)
 /// </summary>
 public struct Engagement
 {
